00c0c9be6cd4d67c8d462757b51db91bd74400ce
Tyler Morrill <Design.Geek@outlook.com>
2018-12-04 20:16:23 -0700

    Add files via upload

---
+++ JS-Types/js-types.js
@@ -0,0 +1,32 @@
+// For this assignment you'll need to concatenate (add together) your first name and last name into a string.
+// Assign the result of the string concatenation to a variable: name 
+ 
+ 
+// Add two numbers together, and assign the result in a variable: calculation 
+// Add a number and a string together. Store the result in a variable: unexpected 
+// What happened?  Why? 
+
+const first = 'Tyler ';
+const middle = 'Jeffery ';
+const last = 'Morrill';
+
+const name = first + middle + last;
+
+console.log(name);
+
+const a = 45;
+const b = 12;
+
+const calculation = a + b;
+
+console.log(calculation);
+const fishCaught = 4;
+const whatType = ' Brown Trout';
+
+const unexpected = fishCaught + whatType;
+
+console.log(unexpected);
+// it concatinated the number and the string and turned them both into a string. because we added them together??
+
+
+
